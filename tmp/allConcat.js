067a0ee4dc06579fe23c0d8450a4dd652a1cc9e9
marynganga <marynganga.mw@gmail.com>
2017-09-07 19:59:14 +0300

    Create an updating time difference function 1. Create a setInterval function. 2. Add an if statement to check if the timediff result is 'a few sec ago'. 3. If above is true, the overlay is displayed and the alarm rings. 4. If false, the result is displayed and the functions updates itself every second.

--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -11,39 +11,55 @@ var Time = require('./../js/alarm.js').timeModule;
    	setInterval(currentTime.update, 1000);
 
   $(document).ready(function() {
-  	
   	$('#setAlarmButton').click(function(event) {  // on clicking the button
   		event.preventDefault();
+  		// Delete previous output
+  		$('.setAlarm').empty();
   		$('.countDown').empty();
+  		
   		var setTime = $('#newAlarm').val(); // insert the inputted time in a variable
   		var alarmDate = new Date(setTime); // parse it into the Date constructor
   		// Parse the new  date object into moment and format the date as desired
   		var alarmTime = moment(alarmDate).format('MMMM, Do YYYY, h:mm:ss a'); 
     	// Display the set alarm time
     	$('.setAlarm').append('<p>The alarm is set for: '+alarmTime + '</p>');
+    	console.log('after inserting new input'+alarmTime);
     	// Clear the input field
     	$('#newAlarm').val('');
 
     	// Calculate the time to alarm ring
    		var countingDown = function(){
-       var timeDiff = moment(alarmDate).fromNow();
+   			var timeDiff = '';
+   			 // console.log('before calculating time diff'+timeDiff);
+   			$('.countDown').empty();
+   			
+       timeDiff = moment(alarmDate).fromNow();
+       console.log('after calculating time diff'+timeDiff);
        var timer = setInterval(function(){
-        // Display time left to the set alarm
-        $('.countDown').empty();
-      $('.countDown').append('<p>Time left : '+timeDiff + '</p>');
+      //  // Display time left to the set alarm
+      // $('.countDown').append('<p>Time left : '+ timeDiff + '</p>');
+      console.log('inside setInterval'+timeDiff);
         if (timeDiff === "a few seconds ago") {
-       $('.countDown').empty();
-       alert('It\'s '+ alarmTime);
-  		
-       console.log('equal');
+        	console.log('after if '+timeDiff);
        $('.countDown').empty();
         $('.countDown').append('<p>'+'Time\'s up!!'+'</p>');
-         clearInterval(timer);
-         var audio = new Audio('./assets/sounds/alarm.mp3');
+        console.log('if diff === a few seconds ago '+timeDiff);
+   	 clearInterval(timer);
+  		// open the overlay section
+        $('.overlay').addClass('open');
+        // Play the alarm audio
+           var audio = new Audio('./assets/sounds/alarm.mp3');
 		audio.play();
+    	// Close the about overlay
+    	$('.closeNav').on('click', function (event) {
+        event.preventDefault();
+        $('.overlay').removeClass('open');
+    	});
       }
       else{
+      	timeDiff = '';
        $('.countDown').empty();
+       timeDiff = moment(alarmDate).fromNow();
        $('.countDown').append('<p>Time left : '+timeDiff + '</p>');
       }
        },1000);
